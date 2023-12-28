e028e8350b229e852ed39abf195fc4e2492b221f
guihendro <ghendro88@gmail.com>
2023-12-28 18:58:51 +0700

    Router setup

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,14 +3,18 @@ import Footer from "../components/Footer";
 import LandingPage from "../pages/LandingPage";
 import Navbar from "../components/Navbar";
 import "../styles/App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <Navbar />
-      <LandingPage />
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/catalog" element={<Catalog />} />
+      </Routes>
       <Footer />
-    </>
+    </BrowserRouter>
   );
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,14 +2,15 @@ import search from "../assets/search.png";
 import cart from "../assets/cart.png";
 import account from "../assets/account.png";
 import "../styles/Navbar.css";
+import { Link } from 'react-router-dom';
 
 export default function Navbar() {
   return (
     <nav className="navbar navbar-expand-lg navbar-custom-bg navbar-dark">
       <div className="container-fluid px-8">
-        <a className="navbar-brand" href="#">
+        <Link to="/" className="navbar-brand">
           da
-        </a>
+        </Link>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
@@ -17,9 +18,9 @@ export default function Navbar() {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto">
             <li className="nav-item">
-              <a className="nav-link" href="#">
+              <Link to="/catalog" className="nav-link">
                 iPhone
-              </a>
+              </Link>
             </li>
             <li className="nav-item">
               <a className="nav-link" href="#">
